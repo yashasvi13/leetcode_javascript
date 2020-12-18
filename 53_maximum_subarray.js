35498595c270279151ccb441e103acd9854289e4
Yashasvi Jain <yashasvijain1303@gmail.com>
2020-12-18 12:51:16 +0530

    max sub array

---
+++ 53_maximum_subarray.js
@@ -0,0 +1,22 @@
+// Approach 1 => Kadane’s Algo
+const maxSubArray = (nums) => {
+  let max = 0;
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (max < sum) {
+      max = sum;
+    } else if (sum < 0) {
+      sum = 0;
+      if (max <= 0) {
+        max = Math.max.apply(Math, nums);
+      }
+    }
+  }
+
+  return max;
+};
+
+console.log(maxSubArray([-1]));
+
+//Approach 2 = Divide and Conquer
