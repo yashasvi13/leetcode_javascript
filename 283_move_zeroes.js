395742c745b0451e5d0bbab8046e2747b06e46ca
Yashasvi Jain <yashasvijain1303@gmail.com>
2020-12-18 13:57:48 +0530

    move zeroes

--- 283_move_zeroes.js
+++ 283_move_zeroes.js
@@ -1,15 +1,11 @@
+//Approach 1
 const moveZeroes = (nums) => {
-  nums.map((num, i) => {
-    if (num === 0) {
-      nums.push(num);
+  for (let i = nums.length; i >= 0; i--) {
+    if (nums[i] === 0) {
       nums.splice(i, 1);
+      nums.push(0);
     }
-    if (nums[i - 1] === 0) {
-      nums.push(nums[i - 1]);
-      nums.splice(i - 1, 1);
-    }
-  });
-
+  }
   return nums;
 };
 
