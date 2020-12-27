60ec730310ab8efa62b0ec9eaf9f249b824b8144
Yashasvi Jain <yashasvijain1303@gmail.com>
2020-12-27 16:27:08 +0530

    merge 2 sorted lists

---
+++ 21_merge_two_sorted_lists.js
@@ -0,0 +1,16 @@
+// function ListNode(val, next) {
+//   this.val = val === undefined ? 0 : val;
+//   this.next = next === undefined ? null : next;
+// }
+
+const mergeTwoLists = (l1, l2) => {
+  if (!l1) return l2;
+  else if (!l2) return l1;
+  else if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
