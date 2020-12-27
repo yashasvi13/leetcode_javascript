98d9e88c1090c8ab0afe488359aca8d599ca9325
Yashasvi Jain <yashasvijain1303@gmail.com>
2020-12-27 15:57:20 +0530

    reverse linked list recursively

--- 206_reverse_linked_list.js
+++ 206_reverse_linked_list.js
@@ -1,3 +1,4 @@
+//iterative
 const reverseList = (head) => {
   let prev = null;
   let current = head;
@@ -10,4 +11,14 @@ const reverseList = (head) => {
     current = next;
   }
   return prev;
+};
+
+//recursive
+
+const reverseList = (head) => {
+  if (!head || !head.next) return head;
+  let reversedLinkedListHead = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return reversedLinkedListHead;
 };
